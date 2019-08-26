249c3a4770580f2dac3eb3a83b15cbf9e3835419
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-08-26 23:52:00 +0300

    Initial commit

---
+++ CsvReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CsvReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Repos\CsvReader\CountryPopulations.csv";
+
+            Reader csvreader = new Reader(filepath);
+
+            Country[] countries = csvreader.ReadFirstNCountries(10);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+        }
+    }
+}
---
+++ CsvReader/Reader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvReader
+{
+    class Reader
+    {
+        private string csvFilePath;
+
+        public Reader(string csvfilepath)
+        {
+            this.csvFilePath = csvfilepath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvline)
+        {
+            string[] parts = csvline.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
